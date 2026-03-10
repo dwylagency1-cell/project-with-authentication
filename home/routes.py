72b34d20fc0aa01f284ba8dce03368e4150fab1c
Dekiru <dwyl.agency1@gmail.com>
2026-03-10 23:57:15 +0530

    routes.py

--- home/routes.py
+++ home/routes.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, url_for, Blueprint
 
 home = Blueprint("home", __name__, template_folder="templates")
 
-@home.routes("/")
+@home.route("/")
 def home_page():
-    return render_template("home.html")
+    return render_template("home/home.html")
+
+
