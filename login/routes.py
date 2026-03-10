9b34cb7560b7bf2fac01794ddc3b4dd1bf966b57
Dekiru <dwyl.agency1@gmail.com>
2026-03-10 23:54:36 +0530

    routes.py

--- login/routes.py
+++ login/routes.py
@@ -1,18 +1,29 @@
 from flask import redirect, url_for, Blueprint, render_template, request
-from blueprint_2.app2 import login_manager
+from flask_login import login_user, logout_user
 from blueprint_2.app2 import bcrypt
 from blueprint_2.app2 import db
+from blueprint_2.signup.models import User
 
 login = Blueprint("login", __name__, template_folder="templates")
 
 @login.route("/login" , methods = ["GET", "POST"])
 def log_in():
     if request.method == 'GET':
-        return render_template("login.html")
+        return render_template("login/login.html")
     
     elif request.method == 'POST':
         password = request.form.get("password")
         username = request.form.get("username")
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("dashboard.dash"))
+        else:
+            return "Invalid username or password"
+
+        
         
 
 
