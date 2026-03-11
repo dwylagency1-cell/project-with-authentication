093132967f7b1623227e329b406fa1f9091fe47a
Dekiru <dwyl.agency1@gmail.com>
2026-03-11 23:54:32 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -9,7 +9,7 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__, template_folder="templates")
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///.app.db"
     app.secret_key = "some_key"
 
@@ -19,11 +19,13 @@ def create_app():
     from blueprint_2.signup.routes import signup
     from blueprint_2.login.routes import login
     from blueprint_2.dashboard.routes import dashboard
+    from blueprint_2.logout.routes import logout
 
     app.register_blueprint(home)
     app.register_blueprint(signup)
     app.register_blueprint(login)
     app.register_blueprint(dashboard)
+    app.register_blueprint(logout)
 
     
     login_manager.init_app(app)
